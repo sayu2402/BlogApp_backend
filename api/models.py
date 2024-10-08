efa636aa4a86242f2fe0de43fd7d7331b722774f
sayooj <sayooj.o12@gmail.com>
2024-10-08 23:08:03 +0530

    User model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.utils.text import slugify
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, null=True, blank= True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def save(self, *args, **kwargs):
+        email_username, mobile = self.email.split('@')
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = email_username
+        if self.username == "" or self.username == None:
+            self.username = email_username
+        
+        super(User, self).save(*args, **kwargs)
