ead0a18729d2b65db0f11ffc5701b81629bf30ca
sayooj <sayooj.o12@gmail.com>
2024-10-09 13:34:48 +0530

    Profile model added

--- api/models.py
+++ api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.utils.text import slugify
+
+
 from shortuuid.django_fields import ShortUUIDField
 import shortuuid
 
@@ -10,9 +12,13 @@ class User(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=100, null=True, blank= True)
+    otp = models.CharField(max_length=100, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
 
     def save(self, *args, **kwargs):
         email_username, mobile = self.email.split('@')
@@ -22,3 +28,25 @@ class User(AbstractUser):
             self.username = email_username
         
         super(User, self).save(*args, **kwargs)
+
+
+class Profile(AbstractUser):
+    user =  models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="image", default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    about = models.CharField(max_length=100, null=True, blank=True)
+    author = models.BooleanField(default=False)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.CharField(max_length=100, null=True, blank=True)
+    twitter = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = self.user.full_name
+        
+        super(Profile, self).save(*args, **kwargs)
--- backend/settings.py
+++ backend/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'api.User'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
