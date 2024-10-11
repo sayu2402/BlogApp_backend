25fbaca8f5c48751b09d7af2bbc95204573746de
sayooj <sayooj.o12@gmail.com>
2024-10-11 13:05:09 +0530

    serializers added

--- api/serializer.py
+++ api/serializer.py
@@ -22,10 +22,103 @@ class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        models = api_models.User
+        model = api_models.User
         fields = ['full_name', 'email', 'password', 'password2']
     
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({'password': "password field didn't match"})
         
+        return attrs
+    
+    def create(self, validated_data):
+        user = api_models.objects.create(
+            full_name = validated_data['full_name'],
+            email = validated_data['email'],
+        )
+
+        email_username, mobile = user.email.split('@')
+        user.username = email_username
+
+        user.set_password(validate_password['password'])
+        user.save()
+
+        return user
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.User
+        field = "__all__"
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Profile
+        field = "__all__"
+
+class CategorySerializer(serializers.ModelSerializer):
+    def get_post_count(self, category):
+        return category.posts.count()
+    
+    class Meta:
+        model = api_models.Category
+        Fields = ["id", "title", "image", "slug", "post_count"]
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Comments
+        Fields = "__all__"
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == "POST":
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Post
+        Fields = "__all__"
+    
+    def __init__(self, *args, **kwargs):
+        super(PostSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == "POST":
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Bookmark
+        Fields = "__all__"
+    
+    def __init__(self, *args, **kwargs):
+        super(BookmarkSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == "POST":
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Notification
+        Fields = "__all__"
+    
+    def __init__(self, *args, **kwargs):
+        super(NotificationSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == "POST":
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+class AuthorSerializer(serializers.Serializer):
+    views = serializers.IntegerField(default=0)
+    posts = serializers.IntegerField(default=0)
+    likes = serializers.IntegerField(default=0)
+    bookmarks = serializers.IntegerField(default=0)
