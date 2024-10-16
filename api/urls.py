26ea15e8a86ebef3ffbf73aca08212cc6eb65bce
sayooj <sayooj.o12@gmail.com>
2024-10-16 13:54:53 +0530

    Dashboard views added

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,22 @@ urlpatterns = [
     path("post/like-post/", api_views.LikePostAPIView.as_view()),
     path("post/comment-post/", api_views.PostCommentAPIView.as_view()),
     path("post/bookmark-post/", api_views.BookmarkPostAPIView.as_view()),
-    path("author/dashboard/stats/<user_id>", api_views.DashboardStats.as_view()),
+    # Dashboard
+    path("author/dashboard/stats/<user_id>/", api_views.DashboardStats.as_view()),
+    path(
+        "author/dashboard/comment-list/<user_id>/",
+        api_views.DashboardCommentLists.as_view(),
+    ),
+    path(
+        "author/dashboard/reply-comment/",
+        api_views.DashboardReplyCommentAPIView.as_view(),
+    ),
+    path(
+        "author/dashboard/noti-list/<user_id>/",
+        api_views.DashboardNotificationLists.as_view(),
+    ),
+    path(
+        "author/dashboard/noti-mark-seen/",
+        api_views.DashboardMarkNotificationAsSeen.as_view(),
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -227,3 +227,65 @@ class DashboardStats(generics.ListAPIView):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class DashboardPostLists(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [SessionAuthentication]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        user = api_models.User.objects.get(id=user_id)
+        return api_models.Post.objects.filter(user=user).order_by("-id")
+
+
+class DashboardCommentLists(generics.ListAPIView):
+    serializer_class = api_serializer.CommentSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [SessionAuthentication]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        user = api_models.User.objects.get(id=user_id)
+        return api_models.Comments.objects.filter(post__user=user)
+
+
+class DashboardNotificationLists(generics.ListAPIView):
+    serializer_class = api_serializer.NotificationSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [SessionAuthentication]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        user = api_models.User.objects.get(id=user_id)
+        return api_models.Notification.objects.filter(seen=False, user=user)
+
+
+class DashboardMarkNotificationAsSeen(APIView):
+
+    def post(self, request):
+        noti_id = request.data["noti_id"]
+        notification = api_models.Notification.objects.get(id=noti_id)
+
+        notification.seen = True
+        notification.save()
+
+        return Response(
+            {"message": "Notification marked as seen"}, status=status.HTTP_200_OK
+        )
+
+
+class DashboardReplyCommentAPIView(APIView):
+
+    def post(self, request):
+        comment_id = request.data["comment_id"]
+        reply = request.data["reply"]
+
+        comment = api_models.Comments.objects.get(id=comment_id)
+        comment.reply = reply
+        comment.save()
+
+        return Response(
+            {"message": "Comment response sent"}, status=status.HTTP_201_CREATED
+        )
